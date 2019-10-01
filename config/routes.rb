41621743f398f2000310334cd2c9c6a23eed54c9
BaronLip <baronlip@gmail.com>
2019-09-30 22:29:39 -0500

    Done.

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,22 @@
 Rails.application.routes.draw do
-  resources :authors, only: %i[show index] do
-    resources :posts, only: %i[show index new edit]
-  end
+    resources :authors, only: %i[show index] do
+        resources :posts, only: %i[show index new edit]
+    end
 
-  resources :posts, only: %i[index show new create edit update]
+    resources :posts, only: %i[index show new create edit update]
 
-  get '/admin/stats', to: 'stats#index'
 
-  root 'posts#index'
+    #-Lab code starts here:
+
+    namespace :admin do
+        resources :stats, only: [:index]
+    end
+
+    # is the equivalent to the below.
+
+    # scope '/admin', module: 'admin' do
+    #     resources :stats, only: [:index]
+    # end
+
+    root 'posts#index'
 end
